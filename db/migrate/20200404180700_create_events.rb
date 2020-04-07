92df0c8563f1e0b987c0277e938dee0c6619ebbf
effgenij <effgenij@gmail.com>
2020-04-07 22:20:17 +0300

    fix migration

--- db/migrate/20200404180700_create_events.rb
+++ db/migrate/20200404180700_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.srting :title
+      t.string :title
       t.text :description
       t.string :address
       t.datetime :datetime
