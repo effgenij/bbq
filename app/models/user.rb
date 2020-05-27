17de48033aa7c58e6bec74ed6ec5db42eb45f68e
effgenij <effgenij@gmail.com>
2020-05-27 20:14:13 +0300

    Added comments and subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
 
   before_validation :set_name, on: :create
+  
+  after_commit :link_subscriptions, on: :create
 
   private
 
   def set_name
     self.name = "Товарищ №#{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+    .update_all(user_id: self.id)
   end
 end
