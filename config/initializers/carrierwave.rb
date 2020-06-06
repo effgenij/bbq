0c5f54ef7ff8094e94e31f8a53a9428ab7bb2050
effgenij <effgenij@gmail.com>
2020-06-06 23:11:37 +0300

    delted region from carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS',
-      region:                ENV['S3_BUCKET_REGION'],
+      provider:              'AWS'      
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
